d691b02cb73b7171c3de3c1f92c6c66a491f998f
piyo02 <alzidni2000@gmail.com>
2020-04-19 19:11:15 +0800

    m

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/olshop/';
-// $config['base_url'] = 'https://olshop.akar-z.com/';
+// $config['base_url'] = 'http://localhost/olshop/';
+$config['base_url'] = 'https://olshop.akar-z.com/';
 
 /*
 |--------------------------------------------------------------------------
